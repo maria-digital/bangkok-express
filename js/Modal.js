3f423cbca327d424cecd37d919ba0ff7362b2cbd
Stanislav Dolgachov <s.dolgachov@gmail.com>
2021-07-26 23:11:51 +0300

    Update urls

--- js/Modal.js
+++ js/Modal.js
@@ -13,7 +13,7 @@ export default class Modal {
     <div class="modal__inner">
       <div class="modal__header">
         <button type="button" class="modal__close">
-          <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+          <img src="assets/images/icons/cross-icon.svg" alt="close-icon" />
         </button>
         <h3 class="modal__title"></h3>
       </div>
